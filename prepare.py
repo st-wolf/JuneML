bdf9e8f8ec91c166f4d465aa23be6aa8c679a795
lucidyan <firesonics@yandex.ru>
2015-06-17 00:47:17 +0300

    refactor prepare.py, wrote bash test's script

--- prepare.py
+++ prepare.py
@@ -1,7 +1,146 @@
 import csv
 import numpy as np
+import os
 
-# Достаем данные из csv файла
+
+"""
+Признаковое описание:
+
+Пока предлагаю остановится на следущем подмножестве признаков:
+pclass, sex, age, fare
+"""
+FEATURES = [
+    'passenger_id', # идентификатор юзера
+    'survived',     # спасли ли бедолагу?
+    'pclass',       # класс билета пассажира (1 = 1st, 2 = 2nd, 3 = 3rd)
+    'name',         # имя
+    'sex',          # половая принадлежность (0 = male, 1 = female)
+    'age',          # возраст
+    'sibsp',        # количество братьев, сестер, супруг и т.д. на борту
+    'parch',        # количество родителей, детей на борту 
+    'ticket',       # номер билета
+    'fare',         # стоимость билета  
+    'cabin',        # место на корабле
+    'embarked',     # порт посадки (C = Cherbourg, Q = Queenstown, S = Southampton)
+]
+
+def convert_row_to_dict(row):
+    """
+    Преобразует строку в словарь
+    @param row параметры обучаемой модели
+    @return dict 
+    """
+    feature_row = {}
+
+    i = 0
+
+    for feature in FEATURES:
+        feature_row[feature] = row[i]
+        i += 1
+
+    return feature_row
+
+def load_training_answers(file_name, path_dir = 'data', relative = True):
+    """
+    @todo Копипаста - плохо
+    Читает ответы учителя из csv
+    Возвращает вектор ответов
+    @param :file_name: имя файла
+    @param :path_dir: путь с файлом (по умолчанию берется из папки data)
+    @param :relative: абсолютный ли относительный путь передан в path_dir?
+    @return numpy.array
+    """
+
+    # Определяем абсолютный путь к файлу
+    if relative:
+        path = os.getcwd()
+    else:
+        path = ''
+
+    file_path = os.path.join(path, path_dir, file_name)
+
+    with open(file_path) as input_file:
+        # csv.reader возвращает объект типа Reader
+        # закрытие файла input_file приводит к уничтожению ридера
+        raw_data = csv.reader(input_file, delimiter = ',')
+
+        next(raw_data) # Опускаем заголовок
+
+        data = []
+
+        IS_SURVIVED = 1 # костыль
+
+        for row in raw_data:
+            data.append(row[IS_SURVIVED])
+
+    return np.array(data)
+
+
+
+
+def load_data_set(file_name, path_dir = 'data', relative = True):
+    """
+    Читает данные из csv
+    Возвращает ndarray с разобранными данными
+    @param :file_name: имя файла
+    @param :path_dir: путь с файлом (по умолчанию берется из папки data)
+    @param :relative: абсолютный ли относительный путь передан в path_dir?
+    @return list
+    """
+
+    # Определяем абсолютный путь к файлу
+    if relative:
+        path = os.getcwd()
+    else:
+        path = ''
+
+    file_path = os.path.join(path, path_dir, file_name)
+
+    with open(file_path) as input_file:
+        # csv.reader возвращает объект типа Reader
+        # закрытие файла input_file приводит к уничтожению ридера
+        raw_data = csv.reader(input_file, delimiter = ',')
+
+        next(raw_data) # Опускаем заголовок
+
+        data = []
+
+        for row in raw_data:
+            data.append(convert_row_to_dict(row))
+
+    return data
+
+def parse_data(data):
+    """ Превращает человеко-понятные данные во feature matrix
+    Пока работаем только с подмножеством (pclass, sex, age, fare)
+    @param np_array data разобранные в load_training_set данные
+    @return numpy.array 
+    """
+    parsed_data = []
+
+    for row in data:
+        # костыль для отсутствующего в датасете возраста
+        if row['age'] == "":
+            continue
+
+        sex = 1 if row['sex'] == 'male' else 0
+
+        parsed_data.append(
+            [int(row['pclass']), 
+            sex,                    # Прости, Господи
+            float(row['age']), 
+            float(row['fare'])
+            ]
+        )
+
+    return np.array(parsed_data)
+
+
+        
+def normalize(x):
+    # Нормализаця. Значения всех признаков переводятся в диапазон [-1,1]
+    return x / np.amax(abs(x), axis = 0)
+
 
 """
 Feature Matrix будем хранить в формате ndarray из библиотеки numpy.
@@ -10,55 +149,14 @@ Feature Matrix будем хранить в формате ndarray из библ
 Все числовые признаки пока примем как есть, остальным присвоим числовые значения.
 """
 
-# Отбор признаков
 
-"""
-Признаковое описание:
+# data = load_data_set('train.csv')
+# answers = load_training_answers('train.csv')
+# f_matrix = parse_data(data)
+# f_n_matrix = normalize(f_matrix)
 
-pclass   : класс билета пассажира (1 = 1st, 2 = 2nd, 3 = 3rd)
-name     : имя
-sex      : половая принадлежность (0 = male, 1 = female)
-age      : возраст
-sibsp    : количество братьев, сестер, супруг и т.д. на борту
-parch    : количество родителей, детей на борту 
-ticket   : номер билета
-fare     : стоимость билета
-cabin    : место на корабле
-embarked : порт посадки (C = Cherbourg, Q = Queenstown, S = Southampton)
 
-Пока предлагаю остановится на следущем подмножестве признаков:
-pclass, sex, age, fare
-"""
-
-def parse_row(row):
-	# Преобразует строку в числа
-	sex = 1 if row[4] == 'male' else 0
-	parsed = [int(row[2]), sex, float(row[5]), float(row[9])]
-	return parsed
-
-def load(file_name):
-	# Читает данные из csv, возвращает X и y в ndarray
-
-	with open('./data/train.csv') as input_file:
-		# csv.reader возвращает объект типа Reader
-		# закрытие файла input_file приводит к уничтожению ридера
-		raw_data = csv.reader(input_file, delimiter = ',')
-		next(raw_data) # Опускаем заголовок
-
-		data = []
-		survived = []
-		for row in raw_data:
-			if all([row[i] != '' for i in [2, 4, 5, 9]]):
-				data.append(parse_row(row))
-				survived.append(int(row[1]))
-
-	X = np.array(data)
-	y = np.array(survived)
-
-	return X, y
-
-def normalize(X):
-	# Нормализаця. Значения всех признаков переводятся в диапазон [-1,1]
-	return X / abs(X).max(axis = 0)
-
-# X, y = load('./data/train.csv')
+# # Делаем вывод нормальным
+# np.set_printoptions(threshold=np.nan) # вывод всей матрицы
+# np.set_printoptions(suppress=True) # человеко-понятные цифры
+# print(f_n_matrix)

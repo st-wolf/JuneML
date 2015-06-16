Gradient descent was fixed. Tests on view and optimize was added. Google code style was applied... sometimes...

learn.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from optimize import GD
+from optimize import gradien_descent
 
 """
 Матрица объекты - признаки хранится в ndarray и имеет вид:
@@ -15,11 +15,18 @@ def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
 def cost(param, X, y, rate = 0):
-	# Функция стоимости
-	# :param: параметры обучаемой модели
-	# :X:     матрциа объекты - признаки
-	# :y:     вектор ответов
-	# :rate:  степень регуляризации
+	"""
+	Вычисление функции стоимости
+
+	Args:
+		param (ndarray): параметры обучаемой модели
+		X (ndarray): матрица объекты - признаки
+		y (ndarray): вектор ответов
+		rate: степень регуляризации
+
+	Return:
+		float: значение функции стоимости
+	"""
 
 	m = len(y)
 	return (1 / m) * (
@@ -28,33 +35,54 @@ def cost(param, X, y, rate = 0):
 		(rate / (2*m)) * np.dot(param[1:], param[1:]) )
 
 def grad(param, X, y, rate = 0):
-	# Градиет функции стоимости
-	# :param: параметры обучаемой модели
-	# :X:     матрица объекты - признаки
-	# :y:     вектор ответов
-	# :rate:  степень регуляризации
+	"""
+	Градиет функции стоимости
+
+	Args:
+		param (ndarray): параметры обучаемой модели
+		X (ndarray): матрица объекты - признаки
+		y (ndarray): вектор ответов
+		rate: степень регуляризации
+
+	Return:
+		ndarray: вектор градиента функции стоимости
+	"""
 
 	m = len(y)
 	return (1 / m) * np.dot(sigmoid(np.dot(X, param)), X) + (
 		(rate / m) * np.hstack((0, param[1:])) )
 
 def teach(X, y):
-	# Обучение параметром модели
-	# :X: матрица объекты - признаки
-	# :y: вектор ответов
+	"""
+	Обучение параметром модели
+
+	Args:
+		X (ndarray): матрица объекты - признаки
+		y (ndarray): вектор ответов
+
+	Reutrns:
+		ndarray: оптимальынй вектор параметров модели
+	"""
 
 	# Степень регуляризации
 	rate = 0
-	return GD(
+	return gradien_descent(
 		lambda param: cost(param, X, y, rate),
 		lambda param: grad(param, X, y, rate),
 		np.zeros(X.shape[1])
 		)
 
 def classify(param, X):
-	# Принятие решения о принадлежности
-	# :param: параметры обучаемой модели
-	# :X:     матрица объекты - признаки (может быть вектором)
+	"""
+	Принятие решения о принадлежности
+	
+	Args:
+		param: параметры обучаемой модели
+		X (ndarray): матрица объекты - признаки
+
+	Reutrns:
+		Bool: принадлежность классу (True: 1, False: 0)
+	"""
 
 	return (np.dot(X, param) >= 0)
 

optimize.py
@@ -1,6 +1,8 @@
 # Тут лежит куча алгоритмов минимизации
 # from scipy.optimize import minimize
 
+from numpy.linalg import norm
+
 # Осваиваю Google Code Style for Python
 
 def gradien_descent(f, grad, x0, maxiter = 100):
@@ -27,7 +29,7 @@ def gradien_descent(f, grad, x0, maxiter = 100):
 	while (n < maxiter) if (maxiter >= 0) else True:
 		n += 1
 		x = xmin - alpha * grad(xmin)
-		if abs(x - xmin) < xtol:
+		if norm(x - xmin) < xtol:
 			return x
 		elif f(x) >= fmin:
 			alpha /= 2

test/test_optimize.py
@@ -1,5 +1,9 @@
+import numpy as np
+
+# Может быть сделать все импорты как from ... import item1, item2 ?
 from unittest import TestCase
 from optimize import gradien_descent
+from numpy.linalg import norm
 
 class TestOptimize(TestCase):
 	def setUp(self):
@@ -8,13 +12,30 @@ class TestOptimize(TestCase):
 			"func": lambda x: x ** 2,
 			"grad": lambda x: 2*x,
 			"init": 1,
-			"min": 0,
+			"xmin": 0,
 			"xtol": 1e-6
 		}
-		self.functions = [f_quad]
+
+		f_3d_quad = {
+			"func": lambda x: (x[0] ** 2) + (x[1] ** 2) + (x[2] ** 2) + 
+				x[0]*x[1] + x[2] + 10,
+			"grad": lambda x: np.array([2*x[0] + x[1], 2*x[1] + x[0], 2*x[2] + 1]),
+			"init": np.array([10, 10, 10]),
+			"xmin": np.array([0, 0, -0.5]),
+			"xtol": 1e-6
+		}
+
+		f_tan_of_squared = {
+			"func": lambda x: np.tan(x ** 2),
+			"grad": lambda x: 2*x / (np.cos(x ** 2) ** 2),
+			"init": np.pi / 4,
+			"xmin": 0,
+			"xtol": 1e-6
+		}
+
+		self.functions = [f_quad, f_3d_quad, f_tan_of_squared]
 
 	def test_gradient_descent(self):
 		for f in self.functions:
-			xmin = gradien_descent(f["func"], f["grad"], f["init"])
-			print(abs(xmin - f["min"]))
-			self.assertFalse(abs(xmin - f["min"]) > f["xtol"])
+			xmin = gradien_descent(f["func"], f["grad"], f["init"], maxiter = 200)
+			self.assertFalse(norm(xmin - f["xmin"]) > f["xtol"])

view.py
@@ -1,11 +1,10 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 def decision_boundary(param, *args):
 	# Рисует проекцию разделяющей поверхности на плоскость двух параметров
 	pass
 
-def classes(x1, x2, y, names = []):
+def show_classes(x1, x2, y, names = []):
 	# Рисует классы на плоскости двух параметров
 
 	# Цветовая схема от Andrew Ng
@@ -19,9 +18,3 @@ def classes(x1, x2, y, names = []):
 
 	plt.grid()
 	plt.show()
-
-# Это нужно оформить в видет тестов
-# x1 = np.array([1,2,3,4])
-# x2 = np.array([2,3,1,4])
-# y  = np.array([1,0,0,1])
-# classes(x1, x2, y, names = ['feature1', 'feature2'])
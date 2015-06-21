7dbd492b968d9cdedfe95c667ad71c9288dac912
lucidyan <firesonics@yandex.ru>
2015-06-22 02:10:11 +0300

    Зарефакторен prepare.py, заведено на тестовых данных

--- prepare.py
+++ prepare.py
@@ -2,97 +2,47 @@ import csv
 import numpy as np
 import os
 
+from collections import namedtuple
 
-"""
-Признаковое описание:
+'''
+Именованный кортеж с признаками
+'''
+Row = namedtuple(
+    'Row', 
+    [
+        'passenger_id', # идентификатор юзера
+        'pclass',       # класс билета пассажира (1 = 1st, 2 = 2nd, 3 = 3rd)
+        'name',         # имя
+        'sex',          # половая принадлежность (0 = male, 1 = female)
+        'age',          # возраст
+        'sibsp',        # количество братьев, сестер, супруг и т.д. на борту
+        'parch',        # количество родителей, детей на борту 
+        'ticket',       # номер билета
+        'fare',         # стоимость билета  
+        'cabin',        # место на корабле
+        'embarked',     # порт посадки (C = Cherbourg, Q = Queenstown, S = Southampton)
+    ]
+)
 
-Пока предлагаю остановится на следущем подмножестве признаков:
-pclass, sex, age, fare
-"""
+'''
+Используемые признаки
+'''
 FEATURES = [
-    'passenger_id', # идентификатор юзера
-    'survived',     # спасли ли бедолагу?
-    'pclass',       # класс билета пассажира (1 = 1st, 2 = 2nd, 3 = 3rd)
-    'name',         # имя
-    'sex',          # половая принадлежность (0 = male, 1 = female)
-    'age',          # возраст
-    'sibsp',        # количество братьев, сестер, супруг и т.д. на борту
-    'parch',        # количество родителей, детей на борту 
-    'ticket',       # номер билета
-    'fare',         # стоимость билета  
-    'cabin',        # место на корабле
-    'embarked',     # порт посадки (C = Cherbourg, Q = Queenstown, S = Southampton)
+    'pclass',
+    'sex',
+    'age', 
+    'fare'
 ]
 
-def convert_row_to_dict(row):
-    """
-    Преобразует строку в словарь
-    @param row параметры обучаемой модели
-    @return dict 
-    """
-    feature_row = {}
-
-    i = 0
-
-    for feature in FEATURES:
-        feature_row[feature] = row[i]
-        i += 1
-
-    return feature_row
-
-def load_training_answers(file_name, path_dir = 'data', relative = True):
-    """
-    @todo Копипаста - плохо
-    Читает ответы учителя из csv
-    Возвращает вектор ответов
-    @param :file_name: имя файла
-    @param :path_dir: путь с файлом (по умолчанию берется из папки data)
-    @param :relative: абсолютный ли относительный путь передан в path_dir?
-    @return numpy.array
-    """
-
-    # Определяем абсолютный путь к файлу
-    if relative:
-        path = os.getcwd()
-    else:
-        path = ''
-
-    file_path = os.path.join(path, path_dir, file_name)
-
-    with open(file_path) as input_file:
-        # csv.reader возвращает объект типа Reader
-        # закрытие файла input_file приводит к уничтожению ридера
-        raw_data = csv.reader(input_file, delimiter = ',')
-
-        next(raw_data) # Опускаем заголовок
-
-        data = []
-
-
-
-        IS_SURVIVED = 1 # костыль
-        AGE = 5 # костыль
-
-        for row in raw_data:
-            # костыль для отсутствующего в датасете возраста
-            if row[AGE] == "":
-                continue
-
-            data.append(int(row[IS_SURVIVED]))
-
-    return np.array(data)
-
-
-
-
-def load_data_set(file_name, path_dir = 'data', relative = True):
+def load_data_set(data_file_name, answers_file_name, path_dir = 'data', relative = True):
     """
     Читает данные из csv
     Возвращает ndarray с разобранными данными
-    @param :file_name: имя файла
+    @param :data_file_name: имя файла с данными
+    @param :answers_file_name: имя файла с ответами
     @param :path_dir: путь с файлом (по умолчанию берется из папки data)
     @param :relative: абсолютный ли относительный путь передан в path_dir?
-    @return list
+    @return (np.array, np.array) Матрицу параметров, Вектор ответов
     """
 
     # Определяем абсолютный путь к файлу
@@ -101,57 +51,85 @@ def load_data_set(file_name, path_dir = 'data', relative = True):
     else:
         path = ''
 
-    file_path = os.path.join(path, path_dir, file_name)
+    # Путь к файлу с данными
+    data_file_path = os.path.join(path, path_dir, data_file_name)
 
-    with open(file_path) as input_file:
-        # csv.reader возвращает объект типа Reader
-        # закрытие файла input_file приводит к уничтожению ридера
-        raw_data = csv.reader(input_file, delimiter = ',')
+    # Путь к файлу с ответами
+    answers_file_path = os.path.join(path, path_dir, answers_file_name)
 
-        next(raw_data) # Опускаем заголовок
+    try:
+        with open(data_file_path, 'r') as data_file, open(answers_file_path, 'r') as answers_file:
+            raw_data = csv.reader(data_file, delimiter = ',')
+            raw_answers = csv.reader(answers_file, delimiter = ',')
 
-        data = []
+            # Опускаем заголовки
+            next(raw_data)
+            next(raw_answers)
 
-        for row in raw_data:
-            data.append(convert_row_to_dict(row))
+            data = []
+            answers = []
+            for row_dat, row_ans in zip(raw_data, raw_answers):
+                dat = Row._make(row_dat) # Создаем объект namedtuple для удобной работы с полями
+                if (check_data_consist(dat)):
+                    vector_data = convert_data_to_vector(dat) # Превращаем данные в необходимого вида вектор
+                    data.append(vector_data)
+                    answers.append(int(row_ans[1])) # Параметр выживания
 
-    return data
+            data = np.array(data)
+            answers = np.array(answers)
 
-def parse_data(data):
-    """ Превращает человеко-понятные данные во feature matrix
+        return (data, answers)
+
+    except IOError as e:
+        print ('Operation failed: %s' % e.strerror)
+
+
+
+def check_data_consist(data):
+    '''
+    Проверяем целостность входных данных
+    @data namedtuple - данные для проверки
+    @return Bool
+    '''
+
+    # Конвертируем в словарь для доступа к индексам
+    data = data._asdict()
+    for feature in FEATURES:
+        if (feature in data) and (data[feature] == ''):
+            return False
+    return True
+
+
+def convert_data_to_vector(data):
+    """ Превращает человеко-понятные данные во вектор значений параметров
     Пока работаем только с подмножеством (pclass, sex, age, fare)
     @param np_array data разобранные в load_training_set данные
     @return numpy.array 
     """
-    parsed_data = []
+    vector_data = []
 
-    for row in data:
-        # костыль для отсутствующего в датасете возраста
-        if row['age'] == "":
-            continue
+    # Конвертируем в словарь для доступа к индексам
+    data = data._asdict()
+    for feature in FEATURES:
+        value = data[feature]
 
-        sex = 1 if row['sex'] == 'male' else 0
+        if feature == 'sex':
+            value = 1 if (value == 'male') else 0
 
-        parsed_data.append(
-            [int(row['pclass']), 
-            sex,                    # Прости, Господи
-            float(row['age']), 
-            float(row['fare'])
-            ]
-        )
+        vector_data.append(float(value))
 
-    return np.array(parsed_data)
+    return vector_data
 
 
         
-def normalize(x):
-    # Нормализаця. Значения всех признаков переводятся в диапазон [-1,1]
-    # добавляется столбец с единицами
-    x_norm = x / np.amax(abs(x), axis = 0)
-    features_len = x_norm.shape[0]
-    x_norm = np.c_[ np.ones(features_len), x_norm  ]
+def normalize(data):
+    '''Нормализаця. Значения всех признаков переводятся в диапазон [-1,1]
+       добавляется столбец с единицами'''
+    data_norm = data / np.amax(abs(data), axis = 0)
+    features_len = data_norm.shape[0]
+    data_norm = np.c_[ np.ones(features_len), data_norm  ]
     
-    return x_norm
+    return data_norm
 
 
 """
--- run.py
+++ run.py
@@ -4,22 +4,40 @@ from learn import *
 import matplotlib.pyplot as plt
 
 
-data = load_data_set('train.csv')
-answers = load_training_answers('train.csv')
-f_matrix = parse_data(data)
-f_n_matrix = normalize(f_matrix)
+# train_data = load_data_set('train.csv')
+# train_answers = load_training_answers('train.csv')
+# f_matrix = parse_data(data)
+# f_n_matrix = normalize(f_matrix)
 
-# Делаем вывод нормальным
-np.set_printoptions(threshold = np.nan) # вывод всей матрицы
-np.set_printoptions(suppress = True) # человеко-понятные цифры
+# # Делаем вывод нормальным
+# np.set_printoptions(threshold = np.nan) # вывод всей матрицы
+# np.set_printoptions(suppress = True) # человеко-понятные цифры
 
-x0 = f_n_matrix[1, :]
+# x0 = f_n_matrix[1, :]
+# optim_feature_m = teach(f_n_matrix, answers, x0)
 
-optim_feature_m = teach(f_n_matrix, answers, x0)
+# print(optim_feature_m)
 
-print(optim_feature_m)
-
-# Лог работы
+# Лог работы по изначальным данным (старая версия)
 # точность: 9.97064402533e-06, 2259 шагов
 # тета: [ 4.98156804 -3.80341945 -2.51632704 -2.93216553  0.2794166 ]
 # время работы:	1m10.601s
+
+
+test_data, test_answers = load_data_set('test.csv', 'gendermodel.csv')
+test_data = normalize(test_data)
+
+# x0 = test_data[1, :]
+# optim_feature_m = teach(test_data, test_answers, x0)
+# print(optim_feature_m)
+
+
+theta = np.array([ 4.98156804, -3.80341945, -2.51632704, -2.93216553,  0.2794166 ])
+classified_data = classify(theta, test_data)
+
+n = 0
+m = len(classified_data)
+for iter in range(0, m - 1):
+	if bool(test_answers[iter]) == classified_data[iter]:
+		n += 1
+print('Процент попаданий - ', n / m * 100)

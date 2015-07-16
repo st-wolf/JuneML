8dfabce8f31f63d3ece798e4ff4b48b798743bdf
JosephTheCat <gloriouslair@gmail.com>
2015-07-16 13:04:37 +0300

    Conjugate gradients was added

--- optimize.py
+++ optimize.py
@@ -1,23 +1,183 @@
 # Тут лежит куча алгоритмов минимизации
 # from scipy.optimize import minimize
 
+import numpy as np
+
 from numpy.linalg import norm
 
-def newtons_method(func, grad, x0, xtol = 1e-10, maxiter = 200):
+
+# Prototype: optimize functon calls, think about merging of zoom and line_search
+def line_search(func, diff, t0, tmax, maxiter = 100):
 	"""
-	Метод Ньютона
+	Алгоритм линейной оптимизации по книге Nocedal, Wright - Numerical optimization.
+	Поиск точки, удовлетворяющей строгим уловиям Вольфе с константами (1e-4, -0.9).
+	Происходит минимизация вдоль некоторого ветора h.
+	Функция: f(t) = f(x + t*h) и производная f'(t) = df / dt определяются снаружи.
+	Для этого создается lambda - функция одного оргумента t.
+
+	Args:
+		func: Функция одного аргумента t - длины шага линейного поиска
+		diff: Производная функции f
+		t0: Первое приближение к оптимальному шагу
+		tmax: Максимальный шаг
+		maxiter: Максимальное число итераций
+
+	Returns:
+		Длина шага, удовлетворяющая строгим условиям Вольфе
+	"""
+
+	# Параметры строгих условий Вольфе
+	c = (1e-4, 0.1)
+
+	# Последовательность приближений к решению задачи
+	t = [0, t0]
+
+	# Счетчик количества итераций
+	i = 1
+
+	while True:
+		if (func(t[i]) > func(0) + c[0] * t[i] * diff(0)) or \
+			(func(t[i]) >= func(t[i-1])):
+
+			return zoom(func, diff, t[i-1], t[i])
+
+		if abs(diff(t[i])) <= c[1] * abs(diff(0)):
+			return t[i]
+
+		if diff(t[i]) >= 0:
+			# Можно поменять t[i] и t[i-1] местами.
+			# В книге реализация выглядит так, чтобы поддерживать выполнение
+			# условия diff(t_low) * (t_hight - t_low) < 0
+			# На мой взгляд, оно исключительно косметическое
+			return zoom(func, diff, t[i], t[i-1])
+
+		tnew = 2 * t[i]
+		if tnew > tmax:
+			tnew = tmax
+
+		t.append(tnew)
+
+		if i == maxiter:
+			# Строгие условия Волфе не выполнены, однако условие убывания выполняется
+			# Вообще, это условие нужно убрать, и так все должно работать.
+			print("Line search: diff = %.15f after %i iterations" % (diff(t[i]), i))
+			return t[i]
+
+		i += 1
+
+
+def zoom(func, diff, t_low, t_hight, maxiter = 100):
+	"""
+	Вспомогательная функция к алгоритму линейной оптимизации
+
+	Args:
+		func: Функция одного аргумента t - длины шага линейного поиска
+		diff: Производная функции f
+		t_low, t_hight: Значения по t между которыми заключена оптимальная длина шага
+		maxiter: Максимальное число итераций
+
+	Returns:
+		Длина шага, удовлетворяющая строгим условиям Вольфе
+	"""
+
+	# Параметры строгих условий Вольфе
+	c = (1e-4, 0.1)
+
+	# Счетчик количества итераций
+	i = 1
+
+	while True:
+		# Пока пользуемся бисекцией, в дальнейшем заменим на интерполяцию
+		# кубическим многочленом
+		t = (t_low + t_hight) / 2
+
+		# print(t_low, t_hight)
+
+		if (func(t) > func(0) + c[0] * t * diff(0)) or (func(t) > func(t_low)):
+			t_hight = t
+		else:
+			if abs(diff(t)) <= -c[1] * diff(0):
+				return t
+			
+			# От этого условия можно избавиться
+			if diff(t) * (t_hight - t_low) >= 0:
+				t_hight = t_low
+
+			t_low = t
+
+		if i == maxiter:
+			print("Zoom: diff = %.15f after %i iterations" % (diff(t), i))
+			return t
+
+		i += 1
+
+
+# Prototype: optimize function calls, use spetial formule for t0
+def conjugate_gradients(func, grad, x0, gtol, maxiter = 200):
+	"""
+	Метод сопряженных градиентов с линейным поиском по правилу Вольфе
 
 	Args:
 		func: Функция одного аргумента типа ndarray
-		grad: Градиент функции f
+		grad: Градиент функции func
 		x0 (ndarray): Начальные условия градиентного спуска
-		xtol : Критерий останова по точности
+		gtol: Критерий останова по малости градиента
+		maxiter (int): Максимальное количество итераций метода
 
 	Returns:
-		ndarray: результат ньютоновской оптимизации
+		(ndarray, ndarray): результат градиентного спуска, массив значений func
+			на итерациях
 	"""
 
-	pass
+	# Размерность задачи - количество сопряженных направлений
+	n = x0.size
+
+	# Последовательность приближений к решению задачи
+	x = [x0]
+
+	# Счетчик количества итераций
+	i = 1
+
+	direction = -grad(x0)
+	offset = 0
+
+	while True:
+		# Линейный поиск вдоль направления direction
+		# В качетсве первого приближения к оптимальному шагу берем 1.
+		# Это нормально для ньютоновских методов, однако авторитетная литература говорит,
+		# что для сопряженных градиентов такое приближение является плохим.
+		# Для сравнения можно поробовать использовать формулу:
+
+		# t0 = step * np.dot(grad(x[i-1]), direction[i-1]) / np.dot(grad(x[i]), direction[i])
+
+		step = line_search(
+			func = lambda t: func(x[i-1] + t * direction),
+			diff = lambda t: np.dot(grad(x[i-1] + t * direction), direction),
+			t0 = 1,
+			tmax = 1e5
+			)
+
+		x.append(x[i-1] + step * direction)
+
+		#print('---', direction, x[i])
+
+		if norm(grad(x[i])) < gtol:
+			return x[i]
+
+		if i == maxiter:
+			print("Conjugent gradients: norm(grad) = %.15f after %i iterations" %
+				(norm(grad(x[i])), i))
+			return x[i]
+
+		# Процедура обновления направления спуска
+		if i % n == 0:
+			direction = -grad(x[i])
+		else:
+			offset = np.dot(grad(x[i]), grad(x[i]) - grad(x[i-1])) / norm(grad(x[i-1])) ** 2
+			direction = -grad(x[i]) + offset * direction
+
+		i += 1
+
 
 def gradient_descent(f, grad, x0, xtol = 1e-10, maxiter = 200):
 	""" 
@@ -28,10 +188,11 @@ def gradient_descent(f, grad, x0, xtol = 1e-10, maxiter = 200):
 		grad: Градиент функции f
 		x0 (ndarray): Начальные условия градиентного спуска
 		xtol : Критерий останова по точности
-		maxiter (int): Максимальное количество итераций метода градиентного спуска.
+		maxiter (int): Максимальное количество итераций метода градиентного спуска
 
 	Returns:
-		(ndarray, ndarray): результат градиентного спуска, массив значений f на итерациях
+		(ndarray, ndarray): результат градиентного спуска, массив значений f
+			на итерациях
 	"""
 
 	alpha = 100
--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -1,18 +1,39 @@
 import numpy as np
-
+import random
 # Может быть сделать все импорты как from ... import item1, item2 ?
-from unittest import TestCase
-from optimize import gradien_descent
+
+from unittest import TestCase, skip
+from optimize import gradient_descent, line_search, conjugate_gradients
 from numpy.linalg import norm
+
+
+# TODO: использовать ООП, Люк.
+def set_direction(f, x0, direction):
+	"""
+	Возвращает функцию, для которой будет выполнен линейный поиск
+	"""
+	return {
+		"func": lambda t: f["func"](x0 + t * direction),
+		"diff": lambda t: np.dot(f["grad"](x0 + t * direction), direction),
+		"init": x0,
+		"direction": direction
+	}
+
+
+# TODO: создать класс Counter, который примет в конструктор фукцию и будет считать количесвто вызовов
+# перегрузить оператор (), передавать вместо функций и посмотреть количество вызовов
+class Counter:
+	pass
+
 
 class TestOptimize(TestCase):
 	def setUp(self):
-		# Определяем набор тестовых функций
+		# Определяем набор тестовых функций для многомерной минимизации
 		f_quad = {
 			"func": lambda x: x ** 2,
 			"grad": lambda x: 2*x,
-			"init": 1,
-			"xmin": 0,
+			"init": np.array(1),
+			"xmin": np.array(0),
 			"xtol": 1e-6
 		}
 
@@ -26,17 +47,64 @@ class TestOptimize(TestCase):
 		}
 
 		f_tan_of_squared = {
-			"func": lambda x: np.tan(x ** 2),
-			"grad": lambda x: 2*x / (np.cos(x ** 2) ** 2),
-			"init": np.pi / 4,
+			"func": lambda x: np.tan(x ** 2) ** 2,
+			"grad": lambda x: 4*x * np.tan(x ** 2) / (np.cos(x ** 2) ** 2),
+			"init": np.array(np.pi / 8),
 			"xmin": 0,
-			"xtol": 1e-4
+			"xtol": 1e-3
 		}
 
+		f_cos_of_sum = {
+			"func": lambda x: np.cos(x[0] + x[1]),
+			"grad": lambda x: np.hstack([-np.sin(x[0] + x[1]), -np.sin(x[0] + x[1])]),
+			"init": np.array([0, 0]),
+			# Нужно использовать fmin вместо xmin
+			# Нужно искользовать ftol вместо xtol
+		}
+
+		# Определяем набор направлений для алгоритма линейного поиска
+		directions = [np.array(1), np.array([1, 1]), np.array([1, 2])]
+
+		# Определяем набор стартовых позиций для алгоритма линейного поиска
+		initials = [np.array(0), np.array([0.1, 0.1]), np.array([10, 10])]
+
+		# Определяем набор тестовых функций для алгоритма линейного поиска
+		f_cos_times_quad = {
+			"func": lambda x: (x ** 2) * np.cos(x),
+			"diff": lambda x: 2 * x * np.cos(x) + (x ** 2) * np.sin(x),
+			"init": 5
+		}
+
+		f_directed_cos_of_sum = set_direction(f_cos_of_sum, initials[1], directions[2])
+
+		self.linear_functions = [f_directed_cos_of_sum]
 		self.functions = [f_quad, f_3d_quad, f_tan_of_squared]
 
+	@skip("Not tested")
 	def test_gradient_descent(self):
 		for f in self.functions:
-			xmin = gradien_descent(f["func"], f["grad"], f["init"], maxiter = 200)
+			xmin = gradient_descent(f["func"], f["grad"], f["init"], maxiter = 200)
+			print(norm(xmin - f["xmin"]))
+			self.assertTrue(norm(xmin - f["xmin"]) < f["xtol"])
+
+	@skip("Not tested")
+	def test_line_search(self):
+		# Пока нет четкого критерия корректности
+		# Проверяем, как хорошо достигаются условия Вольфе
+		# Параметры метода (1e-4, 0.1) забиты наглухо - не надо их менять
+		for f in self.linear_functions:
+			# Первое приближение к искомой длине шага
+			t0 = random.uniform(0, 10)
+			trule = line_search(f["func"], f["diff"], t0, 1000)
+			print("\n")
+			print("t optimal: ", trule)
+			print("Start: ", "Func: ", f["func"](0), "Diff: ", f["diff"](0))
+			print("End: ", "Func: ", f["func"](trule), "Diff: ", f["diff"](trule))
+			print("Point: ", f["init"] + trule * f["direction"])
+
+
+	def test_conjugent_gradients(self):
+		for f in self.functions:
+			xmin = conjugate_gradients(f["func"], f["grad"], f["init"], gtol = 1e-9)
 			print(norm(xmin - f["xmin"]))
 			self.assertTrue(norm(xmin - f["xmin"]) < f["xtol"])
